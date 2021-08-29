06d6c151b6d1413e91e950171b0a13613cf8180e
Nekoyama141592 <mtaky0904@gmail.com>
2021-08-29 22:34:58 +0900

    add_post

--- lib/add_post/add_post_model.dart
+++ lib/add_post/add_post_model.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:flutter_audio_recorder2/flutter_audio_recorder2.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 final addPostProvider = ChangeNotifierProvider(
   (ref) => AddPostModel()
@@ -11,6 +17,14 @@ final addPostProvider = ChangeNotifierProvider(
 class AddPostModel extends ChangeNotifier {
   String postTitle = "";
   bool isLoading = false;
+  late bool isUploading;
+  late bool isRecorded;
+  late bool isRecording;
+  late AudioPlayer audioPlayer;
+  late String filePath;
+  late File audioFile;
+  late FlutterAudioRecorder2 audioRecorder;
+  Recording? current;
   User? currentUser;
   late QuerySnapshot<Map<String, dynamic>> userDocument;
   
@@ -19,6 +33,10 @@ class AddPostModel extends ChangeNotifier {
   }
 
   void init() {
+    isUploading = false;
+    isRecorded = false;
+    isRecording = false;
+    audioPlayer = AudioPlayer();
     setCurrentUser();
   }
 
@@ -31,13 +49,96 @@ class AddPostModel extends ChangeNotifier {
     isLoading = false;
     notifyListeners();
   }
+
+  Future startRecording(BuildContext context) async {
+    final bool? hasRecordingPermission =
+    await FlutterAudioRecorder2.hasPermissions;
+    if (hasRecordingPermission == true) {
+      Directory directory = await getApplicationDocumentsDirectory();
+      String setFilePath = directory.path + '/'
+      + currentUser!.uid
+      +  DateTime.now().millisecondsSinceEpoch.toString() 
+      + '.aac';
+      audioRecorder = FlutterAudioRecorder2(setFilePath, audioFormat: AudioFormat.AAC);
+      await audioRecorder.initialized;
+      audioRecorder.start();
+      current = await audioRecorder.current(channel: 0);
+      filePath = setFilePath;
+      audioFile = File(filePath);
+      notifyListeners();
+    } else {
+      ScaffoldMessenger.of(context)
+      .showSnackBar(SnackBar(content: Center(child: Text('Please enable recording permission'))));
+    }
+  }
+
+  void onRecordButtonPressed(context) async {
+    if (!isRecording) {
+      isRecorded = false;
+      isRecording = true;
+      notifyListeners();
+      await startRecording(context);
+      
+    } else {
+      audioRecorder.stop();
+      isRecording = false;
+      isRecorded = true;
+      notifyListeners();
+    }
+  }
+
+  Future<String> onFileUploadButtonPressed(context) async {
+    FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+    isUploading = true;
+    try {
+      await firebaseStorage
+      .ref('posts')
+      .child(
+        filePath.substring(
+          filePath.lastIndexOf('/'),
+          filePath.length
+        )
+      )
+      
+      .putFile(audioFile);
+    } catch(e) {
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error occured while uploading')
+        )
+      );
+    }
+    final String downloadURL = await firebaseStorage
+    .ref('posts')
+    .child(
+      filePath.substring(
+          filePath.lastIndexOf('/'),
+          filePath.length
+      )
+    )
+    .getDownloadURL();
+    
+    return downloadURL;
+  }
+
+
+  void onRecordAgainButtonPressed() {
+    isRecorded = false;
+    notifyListeners();
+  }
   
-  void addButtonPressed(context) async {
+  void onAddButtonPressed(context) async {
     startLoading();
     await addPostToFirebase(context);
     endLoading();
   }
 
+  Future setCurrentUser() async {
+    currentUser = FirebaseAuth.instance.currentUser;
+  }
+
+  
   Future addPostToFirebase(context) async {
     if (postTitle.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -45,24 +146,25 @@ class AddPostModel extends ChangeNotifier {
       );
     } else {
       try {
+        final audioURL = await onFileUploadButtonPressed(context);
         await FirebaseFirestore.instance.collection('posts')
         .add({
           'imageURL': '',
           'ImageExist': false,
-          'audioURL': '',
+          'audioURL': audioURL,
           'uid': currentUser!.uid,
           'createdAt': Timestamp.now(),
           'updatedAt': Timestamp.now(),
           'score': 0,
           'likes': 0,
           'preservations': 0,
+          'title': postTitle,
         });
+        isUploading = false;
+        notifyListeners();
       } catch(e) {
         print(e.toString());
       }
     }
-  }
-  Future setCurrentUser() async {
-    currentUser = FirebaseAuth.instance.currentUser;
   }
 }
--- lib/add_post/add_post_page.dart
+++ lib/add_post/add_post_page.dart
@@ -8,10 +8,85 @@ class AddPostPage extends ConsumerWidget {
   @override  
   Widget build(BuildContext context, ScopedReader watch) {
     final _addPostProvider = watch(addPostProvider);
+    final postTitleController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text('add'),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            _addPostProvider.isUploading ?
+            Indicator()
+            : AudioButtons(addPostProvider: _addPostProvider),
+            TextField(
+              controller: postTitleController,
+              onChanged: (text) {
+                _addPostProvider.postTitle = text;
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AudioButtons extends StatelessWidget {
+  const AudioButtons({
+    Key? key,
+    required AddPostModel addPostProvider,
+  }) : _addPostProvider = addPostProvider, super(key: key);
+
+  final AddPostModel _addPostProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: Icon(Icons.replay),
+          onPressed: () {
+            _addPostProvider.onRecordAgainButtonPressed();
+          }, 
+        ),
+        IconButton(
+          icon: _addPostProvider.isRecording ?
+          Icon(Icons.pause)
+          : Icon(Icons.fiber_manual_record),
+          onPressed: () async {
+            _addPostProvider.onRecordButtonPressed(context);
+          }, 
+        ),
+        IconButton(
+          icon: Icon(Icons.upload_file),
+          onPressed: () {
+            _addPostProvider.onAddButtonPressed(context);
+          }, 
+        )
+      ],
+    );
+  }
+}
+
+class Indicator extends StatelessWidget {
+  const Indicator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: LinearProgressIndicator(),
+        )
+      ],
     );
   }
 }
