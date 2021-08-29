cc2990bc5c502e385ac327f677c1fb6d987f7871
Nekoyama141592 <mtaky0904@gmail.com>
2021-08-30 00:28:25 +0900

    set_posts_model

--- lib/parts/posts/posts_model.dart
+++ lib/parts/posts/posts_model.dart
@@ -1,2 +1,164 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import 'package:whisper/parts/posts/notifiers/play_button_notifier.dart';
+import 'package:whisper/parts/posts/notifiers/progress_notifier.dart';
+import 'package:whisper/parts/posts/notifiers/repeat_button_notifier.dart';
+
+final postsProvider = ChangeNotifierProvider(
+  (ref) => PostsModel()
+); 
+class PostsModel extends ChangeNotifier {
+  // notifiers
+  final currentSongTitleNotifier = ValueNotifier<String>('');
+  final playlistNotifier = ValueNotifier<List<String>>([]);
+  final progressNotifier = ProgressNotifier();
+  final repeatButtonNotifier = RepeatButtonNotifier();
+  final isFirstSongNotifier = ValueNotifier<bool>(true);
+  final playButtonNotifier = PlayButtonNotifier();
+  final isLastSongNotifier = ValueNotifier<bool>(true);
+  final isShuffleModeEnabledNotifier = ValueNotifier<bool>(false);
+  // just_audio
+  late AudioPlayer audioPlayer;
+  late ConcatenatingAudioSource playlist;
+  // showPage(just_audio)
+  bool isShowModel = false;
+  // FirebaseAuth
+  User? user;
+  // Cloud_Firestore
+  late QuerySnapshot<Map<String, dynamic>> follows;
+  late QuerySnapshot<Map<String, dynamic>> snapshots;
+  int feedsCount = -1;
+  final oneTimeReadCount = 2;
+  bool isLoading = false;
+  List<DocumentSnapshot> documentSnapshots = [];
+  List<String> followUids = [];
+  List<String> mutesUids = [];
+  // refresh
+  RefreshController refreshController = RefreshController(initialRefresh: false);
+  void init() async {
+    audioPlayer = AudioPlayer();
+    setCurrentUser();
+    notifyListeners();
+  }
+
+  void setCurrentUser() {
+    user = FirebaseAuth.instance.currentUser;
+  }
+  void listenForChangesInPlayerState() {
+    audioPlayer.playerStateStream.listen((playerState) {
+      final isPlaying = playerState.playing;
+      final processingState = playerState.processingState;
+      if (processingState == ProcessingState.loading ||
+          processingState == ProcessingState.buffering) {
+        playButtonNotifier.value = ButtonState.loading;
+      } else if (!isPlaying) {
+        playButtonNotifier.value = ButtonState.paused;
+      } else if (processingState != ProcessingState.completed) {
+        playButtonNotifier.value = ButtonState.playing;
+      } else {
+        audioPlayer.seek(Duration.zero);
+        audioPlayer.pause();
+      }
+    });
+  }
+
+  void listenForChangesInPlayerPosition() {
+    audioPlayer.positionStream.listen((position) {
+      final oldState = progressNotifier.value;
+      progressNotifier.value = ProgressBarState(
+        current: position,
+        buffered: oldState.buffered,
+        total: oldState.total,
+      );
+    });
+  }
+
+  void listenForChangesInBufferedPosition() {
+    audioPlayer.bufferedPositionStream.listen((bufferedPosition) {
+      final oldState = progressNotifier.value;
+      progressNotifier.value = ProgressBarState(
+        current: oldState.current,
+        buffered: bufferedPosition,
+        total: oldState.total,
+      );
+    });
+  }
+
+  void listenForChangesInTotalDuration() {
+    audioPlayer.durationStream.listen((totalDuration) {
+      final oldState = progressNotifier.value;
+      progressNotifier.value = ProgressBarState(
+        current: oldState.current,
+        buffered: oldState.buffered,
+        total: totalDuration ?? Duration.zero,
+      );
+    });
+  }
+
+  void listenForChangesInSequenceState() {
+    audioPlayer.sequenceStateStream.listen((sequenceState) {
+      if (sequenceState == null) return;
+      // TODO: update current song title
+      final currentItem = sequenceState.currentSource;
+      final title = currentItem?.tag as String?;
+      currentSongTitleNotifier.value = title ?? '';
+      // TODO: update playlist
+      final playlist = sequenceState.effectiveSequence;
+      final titles = playlist.map((item) => item.tag as String).toList();
+      playlistNotifier.value = titles;
+      // TODO: update shuffle mode
+      isShuffleModeEnabledNotifier.value = 
+      sequenceState.shuffleModeEnabled;
+      // TODO: update previous and next buttons
+      if (playlist.isEmpty || currentItem == null) {
+        isFirstSongNotifier.value = true;
+        isLastSongNotifier.value = true; 
+      } else {
+        isFirstSongNotifier.value = playlist.first == currentItem;
+        isLastSongNotifier.value = playlist.last == currentItem;
+      }
+    });
+    notifyListeners();
+  }
+
+  void play() async {
+    audioPlayer.play();
+  }
+
+  void pause() {
+    audioPlayer.pause();
+  }
+
+  void seek(Duration position) {
+    audioPlayer.seek(position);
+  }
+  // showPage
+  void onRepeatButtonPressed() {
+    repeatButtonNotifier.nextState();
+    switch (repeatButtonNotifier.value) {
+      case RepeatState.off:
+        audioPlayer.setLoopMode(LoopMode.off);
+        break;
+      case RepeatState.repeatSong:
+        audioPlayer.setLoopMode(LoopMode.one);
+        break;
+      case RepeatState.repeatPlaylist:
+        audioPlayer.setLoopMode(LoopMode.all);
+    }
+  }
+
+  void onPreviousSongButtonPressed() {
+    audioPlayer.seekToPrevious();
+  }
+
+  void onNextSongButtonPressed() {
+    audioPlayer.seekToNext();
+  }
+
+}
