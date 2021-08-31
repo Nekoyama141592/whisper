b27c9c7b36253df992f829d0b77697c69f06cf2b
Nekoyama141592 <mtaky0904@gmail.com>
2021-08-31 21:02:51 +0900

    add_image

--- lib/auth/signup/signup_model.dart
+++ lib/auth/signup/signup_model.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:whisper/constants/routes.dart' as routes;
 
@@ -11,10 +15,53 @@ final signupProvider = ChangeNotifierProvider(
 );
 
 class SignupModel extends ChangeNotifier {
+  String userName = "";
   String email = "";
   String password = "";
   late UserCredential result;
   late User? user;
+  // image
+  bool isLoading = false;
+  XFile? xfile;
+  late File imageFile;
+  String downloadURL = '';
+  startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future showImagePicker() async {
+    final ImagePicker _picker = ImagePicker();
+    xfile = await _picker.pickImage(source: ImageSource.gallery);
+    imageFile = File(xfile!.path);
+    notifyListeners();
+  }
+
+  Future uploadImage() async {
+    if (userName.isEmpty) {
+      print('userNameを入力してください');
+    }
+    try {
+      await FirebaseStorage.instance
+      .ref()
+      .child('users')
+      .child('$userName' + '.png')
+      .putFile(imageFile);
+      downloadURL = await FirebaseStorage.instance
+      .ref()
+      .child('users')
+      .child('$userName' + '.png')
+      .getDownloadURL();
+    } catch(e) {
+      print(e.toString());
+    }
+    return downloadURL;
+  }
 
   Future signup(context) async {
     try{
@@ -37,12 +84,15 @@ class SignupModel extends ChangeNotifier {
     }
   }
   Future addUserToFireStore(uid) async {
+    final imageURL = await uploadImage();
     await FirebaseFirestore.instance
     .collection('users').add({
       'email': email,
       'uid' : uid,
       'subUserName': uid,
       'point': 0,
+      'imageURL': imageURL,
+      'userName': userName,
     });
   }
 }
--- lib/auth/signup/signup_page.dart
+++ lib/auth/signup/signup_page.dart
@@ -10,37 +10,59 @@ class SignupPage extends ConsumerWidget {
     final _signupProvider = watch(signupProvider);
     final emailInputController = TextEditingController();
     final passwordInputController = TextEditingController();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('新規登録'),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            controller: emailInputController,
-            onChanged: (text) {
-              _signupProvider.email = text;
-            },
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: Text('新規登録'),
           ),
-          TextField(
-            controller: passwordInputController,
-            onChanged: (text) {
-              _signupProvider.password = text;
-            },
-            obscureText: true,
+          body: Column(
+            children: [
+              InkWell(
+                child: Container(
+                  width: 100,
+                  height: 160,
+                  child: _signupProvider.xfile != null ?
+                  Image.file(_signupProvider.imageFile)
+                  : Icon(Icons.image, size: 100,),
+                ),
+                onTap: () async {
+                  await _signupProvider.showImagePicker();
+                },
+              ),
+              TextField(
+                controller: emailInputController,
+                onChanged: (text) {
+                  _signupProvider.email = text;
+                },
+              ),
+              TextField(
+                controller: passwordInputController,
+                onChanged: (text) {
+                  _signupProvider.password = text;
+                },
+                obscureText: true,
+              ),
+              
+              SizedBox(height: 24),
+              Center(
+                child: ElevatedButton(
+                  child: Text('signup'),
+                  onPressed: () async {
+                    await _signupProvider.signup(context);
+                  },
+                ),
+              ),
+            ]
           ),
-          
-          SizedBox(height: 24),
-          Center(
-            child: ElevatedButton(
-              child: Text('signup'),
-              onPressed: () async {
-                await _signupProvider.signup(context);
-              },
-            ),
-          ),
-        ]
-      ),
+        ),
+        _signupProvider.isLoading ?
+        Container(
+          color: Colors.grey.withOpacity(0.7),
+          child: Center(child: CircularProgressIndicator(),),
+        )
+        : SizedBox()
+      ],
     );
   }
 }
