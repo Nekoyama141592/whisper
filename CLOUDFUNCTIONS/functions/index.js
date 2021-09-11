d6d3680fcc438a4901c3b6ae8923da094df65354
Nekoyama141592 <mtaky0904@gmail.com>
2021-09-11 20:03:34 +0900

    functions

---
+++ CLOUDFUNCTIONS/functions/index.js
@@ -0,0 +1,51 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
+const algoliasearch = require('algoliasearch');
+const ALGOLIA_APP_ID = functions.config().algoria.app_id
+const ALGOLIA_SEARCH_API_KEY = functions.config.algoria.api_key
+const ALGOLIA_ADMIN_API_KEY = functions.config.algoria.admin_api_key
+const ALGOLIA_INDEX_NAME = "posts";
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY)
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.createPost = functions.firestore
+.document('posts/{id}')
+.onCreate(
+    async (snap,context) => {
+        const newValue = snap.data();
+        newValue.objectID = snap.id;
+        var index = client.initIndex(ALGOLIA_INDEX_NAME);
+        index.saveObject(newValue);
+        console.log('finished')
+    }
+);
+
+exports.updatePost = functions.firestore
+.document('posts/{id}')
+.onUpdate(
+    async (snap, context) => {
+        const afterUpdate = snap.after.data();
+        afterUpdate.objectID = snap.after.id;
+
+        var index = client.initIndex(ALGOLIA_INDEX_NAME);
+        index.saveObject(afterUpdate);
+    }
+);
+
+exports.deletePost = functions.firestore
+.document('posts/{id}')
+.onDelete(
+    async (snap, context) => {
+        const oldID = snap.id;
+        var index = client.initIndex(ALGOLIA_INDEX_NAME);
+        index.deleteObject(oldID);
+    }
+);
+
