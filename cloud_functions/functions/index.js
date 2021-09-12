1ad316d1404a910568f798245fd11881e6eff18e
Nekoyama141592 <mtaky0904@gmail.com>
2021-09-12 22:12:50 +0900

    algolia

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -2,10 +2,10 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase)
 const algoliasearch = require('algoliasearch');
-const ALGOLIA_APP_ID = functions.config().algoria.app_id
-const ALGOLIA_SEARCH_API_KEY = functions.config.algoria.api_key
-const ALGOLIA_ADMIN_API_KEY = functions.config.algoria.admin_api_key
-const ALGOLIA_INDEX_NAME = "posts";
+const ALGOLIA_APP_ID = functions.config().algolia.app_id
+// const ALGOLIA_SEARCH_API_KEY = functions.config().algolia.api_key
+const ALGOLIA_ADMIN_API_KEY = functions.config().algolia.admin_api_key
+const ALGOLIA_INDEX_NAME = "Posts";
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY)
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -20,7 +20,8 @@ exports.createPost = functions.firestore
 .onCreate(
     async (snap,context) => {
         const newValue = snap.data();
-        newValue.objectID = snap.id;
+        // newValue.objectID = snap.id;
+        newValue.objectID = context.params.id
         var index = client.initIndex(ALGOLIA_INDEX_NAME);
         index.saveObject(newValue);
         console.log('finished')
