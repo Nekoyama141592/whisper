7914c93cb12fde87bbf5706d10f7f974307893cb
Nekoyama141592 <mtaky0904@gmail.com>
2021-10-10 22:37:16 +0900

    UI

--- lib/posts/components/details/post_card.dart
+++ lib/posts/components/details/post_card.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 // package
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 // constants
 import 'package:whisper/constants/colors.dart';
 // components
@@ -18,32 +19,63 @@ class PostCard extends StatelessWidget {
 
   @override  
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: kPrimaryColor.withOpacity(0.1),
-            blurRadius: 20,
-            offset: Offset(0, 5)
-          )
-        ]
-      ),
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              leading: UserImage(userImageURL: postDoc['userImageURL'], length: 50.0, padding: 0.0),
-              title: Text(postDoc['userName']),
-              subtitle: Text(
-                postDoc['title'],
-                style: TextStyle(
-                  color: Theme.of(context).focusColor,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      actions: [
+        IconSlideAction(
+          caption: 'Archive',
+          color: Colors.transparent,
+          icon: Icons.archive,
+          
+          onTap: () => print("archive"),
+        ),
+        IconSlideAction(
+          caption: 'Share',
+          color: Colors.transparent,
+          icon: Icons.share,
+          onTap: () => print('Share'),
+        ),
+        IconSlideAction(
+          caption: 'More',
+          color: Colors.transparent,
+          icon: Icons.more_horiz,
+          onTap: () => print('More'),
+        ),
+        IconSlideAction(
+          caption: 'Delete',
+          color: Colors.transparent,
+          icon: Icons.delete,
+          onTap: () => print('Delete'),
+        )
+      ],
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: kPrimaryColor.withOpacity(0.1),
+              blurRadius: 20,
+              offset: Offset(0, 5)
             )
-          ],
+          ]
+        ),
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: UserImage(userImageURL: postDoc['userImageURL'], length: 50.0, padding: 0.0),
+                title: Text(postDoc['userName']),
+                subtitle: Text(
+                  postDoc['title'],
+                  style: TextStyle(
+                    color: Theme.of(context).focusColor,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
