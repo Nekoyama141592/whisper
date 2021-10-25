7ccfab6c9df7158e776c2621b4067586453876e9
Nekoyama141592 <mtaky0904@gmail.com>
2021-10-25 22:59:55 +0900

    link

--- lib/auth/signup/signup_model.dart
+++ lib/auth/signup/signup_model.dart
@@ -168,6 +168,7 @@ class SignupModel extends ChangeNotifier {
       'likeNotifications': [],
       'likedReplys': [],
       'likes': [],
+      'link': '',
       'mutesUids': [],
       'readPosts': [],
       'replyNotifications': [],
