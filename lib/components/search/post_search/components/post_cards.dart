741134c204b10890af3673ba66f663cb7694b069
Nekoyama141592 <mtaky0904@gmail.com>
2021-11-07 18:22:31 +0900

    UI

--- lib/components/search/post_search/components/post_cards.dart
+++ lib/components/search/post_search/components/post_cards.dart
@@ -7,6 +7,7 @@ import 'package:whisper/constants/routes.dart' as routes;
 // components
 import 'package:whisper/posts/components/details/post_card.dart';
 import 'package:whisper/posts/components/audio_window/audio_window.dart';
+import 'package:whisper/components/search/post_search/components/search_input_field.dart';
 // model
 import 'package:whisper/main_model.dart';
 import 'package:whisper/components/search/post_search/post_search_model.dart';
@@ -27,88 +28,107 @@ class PostCards extends ConsumerWidget {
   
   @override 
   Widget build(BuildContext context, ScopedReader watch) {
-
-    final size = MediaQuery.of(context).size;
+    
     final editPostInfoModel = watch(editPostInfoProvider);
+    final searchController = TextEditingController.fromValue(
+      TextEditingValue(
+        text: postSearchModel.searchTerm,
+        selection: TextSelection.collapsed(
+          offset: postSearchModel.searchTerm.length
+        )
+      )
+    );
 
     return
-    results.isNotEmpty ?
-    SizedBox(
-      height: size.height * 0.65,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: results.length,
-              itemBuilder: (BuildContext context, int i) {
-                final Map<String,dynamic> post = results[i];
-                return 
-                PostCard(
-                  post: post,
-                  onDeleteButtonPressed: () { postSearchModel.onDeleteButtonPressed(context, results[i], mainModel.currentUserDoc, i); },
-                  initAudioPlayer: () async {
-                    await postSearchModel.initAudioPlayer(i);
-                  },
-                  muteUser: () async {
-                    await postSearchModel.muteUser(mainModel.mutesUids, post['uid'], mainModel.prefs, i, mainModel.currentUserDoc);
-                  },
-                  mutePost: () async {
-                    await postSearchModel.mutePost(mainModel.mutesPostIds, post['postId'], mainModel.prefs, i);
-                  },
-                  blockUser: () async {
-                    await postSearchModel.blockUser(mainModel.currentUserDoc, mainModel.blockingUids, post['uid'], i);
-                  },
-                  mainModel: mainModel,
-                );
-              }
-            )
+    Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SearchInputField(
+          searchModel: postSearchModel, 
+          controller: searchController, 
+          press: () async {
+            await postSearchModel.operation(mainModel.mutesUids,mainModel.mutesPostIds,mainModel.blockingUids);
+          }
+        ),
+        results.isNotEmpty ?
+        Flexible(
+          flex: 10,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: results.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    final Map<String,dynamic> post = results[i];
+                    return 
+                    PostCard(
+                      post: post,
+                      onDeleteButtonPressed: () { postSearchModel.onDeleteButtonPressed(context, results[i], mainModel.currentUserDoc, i); },
+                      initAudioPlayer: () async {
+                        await postSearchModel.initAudioPlayer(i);
+                      },
+                      muteUser: () async {
+                        await postSearchModel.muteUser(mainModel.mutesUids, post['uid'], mainModel.prefs, i, mainModel.currentUserDoc);
+                      },
+                      mutePost: () async {
+                        await postSearchModel.mutePost(mainModel.mutesPostIds, post['postId'], mainModel.prefs, i);
+                      },
+                      blockUser: () async {
+                        await postSearchModel.blockUser(mainModel.currentUserDoc, mainModel.blockingUids, post['uid'], i);
+                      },
+                      mainModel: mainModel,
+                    );
+                  }
+                )
+              ),
+              AudioWindow(
+                route: () {
+                  routes.toPostShowPage(
+                    context,
+                    postSearchModel.speedNotifier,
+                    () async { await postSearchModel.speedControll(); },
+                    postSearchModel.currentSongMapNotifier, 
+                    postSearchModel.progressNotifier, 
+                    postSearchModel.seek, 
+                    postSearchModel.repeatButtonNotifier, 
+                    () { postSearchModel.onRepeatButtonPressed(); }, 
+                    postSearchModel.isFirstSongNotifier, 
+                    () { postSearchModel.onPreviousSongButtonPressed(); }, 
+                    postSearchModel.playButtonNotifier, 
+                    () { postSearchModel.play(mainModel.readPostIds, mainModel.readPosts, mainModel.currentUserDoc); }, 
+                    () { postSearchModel.pause(); }, 
+                    postSearchModel.isLastSongNotifier, 
+                    () { postSearchModel.onNextSongButtonPressed(); },
+                    () {
+                      postSearchModel.pause();
+                      routes.toCommentsPage(context, () { postSearchModel.showSortDialogue(context); }, postSearchModel.audioPlayer, postSearchModel.currentSongMapCommentsNotifier, postSearchModel.currentSongMapNotifier, mainModel);
+                    },
+                    () {
+                      postSearchModel.pause();
+                      editPostInfoModel.isEditing = true;
+                      editPostInfoModel.reload();
+                    },
+                    mainModel
+                  );
+                }, 
+                progressNotifier: postSearchModel.progressNotifier, 
+                seek: postSearchModel.seek, 
+                currentSongMapNotifier: postSearchModel.currentSongMapNotifier ,
+                playButtonNotifier: postSearchModel.playButtonNotifier, 
+                play: () { postSearchModel.play(mainModel.readPostIds, mainModel.readPosts, mainModel.currentUserDoc); }, 
+                pause: () { postSearchModel.pause(); }, 
+                currentUserDoc: mainModel.currentUserDoc,
+                isFirstSongNotifier: postSearchModel.isFirstSongNotifier,
+                onPreviousSongButtonPressed: () { postSearchModel.onPreviousSongButtonPressed(); },
+                isLastSongNotifier: postSearchModel.isLastSongNotifier,
+                onNextSongButtonPressed: () { postSearchModel.onNextSongButtonPressed(); },
+                mainModel: mainModel,
+              )
+            ],
           ),
-          AudioWindow(
-            route: () {
-              routes.toPostShowPage(
-                context,
-                postSearchModel.speedNotifier,
-                () async { await postSearchModel.speedControll(); },
-                postSearchModel.currentSongMapNotifier, 
-                postSearchModel.progressNotifier, 
-                postSearchModel.seek, 
-                postSearchModel.repeatButtonNotifier, 
-                () { postSearchModel.onRepeatButtonPressed(); }, 
-                postSearchModel.isFirstSongNotifier, 
-                () { postSearchModel.onPreviousSongButtonPressed(); }, 
-                postSearchModel.playButtonNotifier, 
-                () { postSearchModel.play(mainModel.readPostIds, mainModel.readPosts, mainModel.currentUserDoc); }, 
-                () { postSearchModel.pause(); }, 
-                postSearchModel.isLastSongNotifier, 
-                () { postSearchModel.onNextSongButtonPressed(); },
-                () {
-                  postSearchModel.pause();
-                  routes.toCommentsPage(context, () { postSearchModel.showSortDialogue(context); }, postSearchModel.audioPlayer, postSearchModel.currentSongMapCommentsNotifier, postSearchModel.currentSongMapNotifier, mainModel);
-                },
-                () {
-                  postSearchModel.pause();
-                  editPostInfoModel.isEditing = true;
-                  editPostInfoModel.reload();
-                },
-                mainModel
-              );
-            }, 
-            progressNotifier: postSearchModel.progressNotifier, 
-            seek: postSearchModel.seek, 
-            currentSongMapNotifier: postSearchModel.currentSongMapNotifier ,
-            playButtonNotifier: postSearchModel.playButtonNotifier, 
-            play: () { postSearchModel.play(mainModel.readPostIds, mainModel.readPosts, mainModel.currentUserDoc); }, 
-            pause: () { postSearchModel.pause(); }, 
-            currentUserDoc: mainModel.currentUserDoc,
-            isFirstSongNotifier: postSearchModel.isFirstSongNotifier,
-            onPreviousSongButtonPressed: () { postSearchModel.onPreviousSongButtonPressed(); },
-            isLastSongNotifier: postSearchModel.isLastSongNotifier,
-            onNextSongButtonPressed: () { postSearchModel.onNextSongButtonPressed(); },
-            mainModel: mainModel,
-          )
-        ],
-      ),
-    ) : SizedBox.shrink();
+        ) : SizedBox.shrink(),
+      ],
+    );
   }
 }
--- lib/components/search/post_search/post_search_page.dart
+++ lib/components/search/post_search/post_search_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // components
 import 'package:whisper/details/loading.dart';
 import 'package:whisper/components/search/post_search/components//post_cards.dart';
-import 'package:whisper/components/search/post_search/components/search_input_field.dart';
 // model
 import 'post_search_model.dart';
 import 'package:whisper/main_model.dart';
@@ -24,33 +23,13 @@ class PostSearchPage extends ConsumerWidget {
   @override 
   Widget build(BuildContext context, ScopedReader watch) {
     final searchModel = watch(postSearchProvider);
-    final searchController = TextEditingController.fromValue(
-      TextEditingValue(
-        text: searchModel.searchTerm,
-        selection: TextSelection.collapsed(
-          offset: searchModel.searchTerm.length
-        )
-      )
-    );
-    return Column(
-      children: [
-        SearchInputField(
-          searchModel: searchModel, 
-          controller: searchController, 
-          press: () async {
-            await searchModel.operation(mainModel.mutesUids,mainModel.mutesPostIds,mainModel.blockingUids);
-          }
-        ),
-        searchModel.isLoading ?
-        Loading() 
-        : Expanded(
-          child: PostCards(
-            results: searchModel.results,
-            mainModel: mainModel,
-            postSearchModel: postSearchModel,
-          ),
-        )
-      ],
+    
+    return searchModel.isLoading ?
+    Loading() 
+    : PostCards(
+      results: searchModel.results,
+      mainModel: mainModel,
+      postSearchModel: postSearchModel,
     );
   }
 }
