bef397ad8ec857cb66377eb4459a1433bdeea2a7
Nekoyama141592 <mtaky0904@gmail.com>
2021-11-07 18:42:22 +0900

    UI

--- lib/components/search/user_search/components/user_list.dart
+++ lib/components/search/user_search/components/user_list.dart
@@ -4,28 +4,53 @@ import 'package:flutter/material.dart';
 import 'package:algolia/algolia.dart';
 // components
 import 'package:whisper/components/search/user_search/components/user_result.dart';
+import 'package:whisper/components/search/user_search/components/search_input_field.dart';
 // model
 import 'package:whisper/main_model.dart';
+import 'package:whisper/components/search/user_search/user_search_model.dart';
 
 class UserList extends StatelessWidget {
 
   UserList({
     Key? key,
     required this.results,
-    required this.mainModel
+    required this.mainModel,
+    required this.userSearchModel
   }) : super(key: key);
 
   final List<AlgoliaObjectSnapshot> results;
   final MainModel mainModel;
+  final UserSearchModel userSearchModel;
   
   @override 
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: results.length,
-        itemBuilder: (BuildContext context, int i) =>
-        UserResult(result: results[i].data, mainModel: mainModel)
+
+    final searchController = TextEditingController.fromValue(
+      TextEditingValue(
+        text: userSearchModel.searchTerm,
+        selection: TextSelection.collapsed(
+          offset: userSearchModel.searchTerm.length
+        )
       )
+    );
+
+    return Column(
+      children: [
+        SearchInputField(
+          searchModel: userSearchModel, 
+          controller: searchController, 
+          press: () async {
+            await userSearchModel.operation(mainModel.mutesUids,mainModel.blockingUids);
+          }
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: results.length,
+            itemBuilder: (BuildContext context, int i) =>
+            UserResult(result: results[i].data, mainModel: mainModel)
+          )
+        ),
+      ],
     );
   }
 }
--- lib/components/search/user_search/user_search_page.dart
+++ lib/components/search/user_search/user_search_page.dart
@@ -5,10 +5,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // components
 import 'package:whisper/details/loading.dart';
 import 'package:whisper/components/search/user_search/components/user_list.dart';
-import 'package:whisper/components/search/user_search/components/search_input_field.dart';
 // model
-import 'user_search_model.dart';
 import 'package:whisper/main_model.dart';
+import 'package:whisper/components/search/user_search/user_search_model.dart';
 
 class UserSearchPage extends ConsumerWidget {
 
@@ -20,28 +19,10 @@ class UserSearchPage extends ConsumerWidget {
   final MainModel mainModel;
   @override 
   Widget build(BuildContext context, ScopedReader watch) {
-    final searchModel = watch(searchProvider);
-    final results = searchModel.results;
-    final searchController = TextEditingController.fromValue(
-      TextEditingValue(
-        text: searchModel.searchTerm,
-        selection: TextSelection.collapsed(
-          offset: searchModel.searchTerm.length
-        )
-      )
-    );
-    return Column(
-      children: [
-        SearchInputField(
-          searchModel: searchModel, 
-          controller: searchController, 
-          press: () async {
-            await searchModel.operation(mainModel.mutesUids,mainModel.blockingUids);
-          }
-        ),
-        searchModel.isLoading ?
-        Loading() : UserList(results: results, mainModel: mainModel)
-      ],
-    );
+    final userSearchModel = watch(searchProvider);
+    final results = userSearchModel.results;
+    
+    return userSearchModel.isLoading ?
+    Loading() : UserList(results: results, mainModel: mainModel,userSearchModel: userSearchModel,);
   }
 }
