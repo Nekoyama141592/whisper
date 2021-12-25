ee85d4f4296f619946a60c0efcb08a42b00600c7
Nekoyama141592 <mtaky0904@gmail.com>
2021-12-25 12:46:17 +0900

    s

--- lib/components/user_show/components/details/link_button.dart
+++ lib/components/user_show/components/details/link_button.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
-// package
+// packages
 import 'package:clipboard/clipboard.dart';
 import 'package:url_launcher/url_launcher.dart';
 
