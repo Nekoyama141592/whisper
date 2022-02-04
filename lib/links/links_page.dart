3bb1c020cf21a8b1ded62d8e7c06de86dc88307c
Nekoyama141592 <mtaky0904@gmail.com>
2022-02-04 21:49:28 +0900

    whisperLink

--- lib/links/links_page.dart
+++ lib/links/links_page.dart
@@ -38,23 +38,34 @@ class LinksPage extends StatelessWidget {
               child: ListView.builder(
                 itemCount: whisperLinksOfModel.length,
                 itemBuilder: (BuildContext context, int i) {
-                  final TextEditingController labelEditingController = TextEditingController(text: whisperLinksOfModel[i].label );
-                  final TextEditingController linkEditingController = TextEditingController(text: whisperLinksOfModel[i].link);
+                  final whisperLink = whisperLinksOfModel[i];
+                  final TextEditingController labelEditingController = TextEditingController(text: whisperLink.label );
+                  final TextEditingController linkEditingController = TextEditingController(text: whisperLink.link);
                   return Padding(
                     padding: EdgeInsets.all(size.height/64.0),
                     child: Column(
                       children: [
+                        Text('ラベル'),
                         TextFormField(
                           decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.delete)
+                            suffixIcon: Icon(Icons.delete),
+                            hintText: '公式サイト'
                           ),
                           controller: labelEditingController,
+                          onChanged: (text) {
+                            whisperLink.label = text;
+                          },
                         ),
+                        Text('URL'),
                         TextFormField(
                           decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.delete)
+                            suffixIcon: Icon(Icons.delete),
+                            hintText: 'https://'
                           ),
-                          controller: linkEditingController
+                          controller: linkEditingController,
+                          onChanged: (text) {
+                            whisperLink.link = text;
+                          },
                         ),
                       ],
                     ),
