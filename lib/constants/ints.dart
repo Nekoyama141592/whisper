1ad356d241ab622fa87a1c0828db83e880c84f77
Nekoyama141592 <mtaky0904@gmail.com>
2022-03-13 12:04:40 +0900

    調整

--- lib/constants/ints.dart
+++ lib/constants/ints.dart
@@ -3,10 +3,10 @@ const int oneTimeReadCount = 30;
 const int topFlashDisplaySeconds = 4;
 const int commentScrollFlashBarConstantCount = 2;
 const int dialogueMilliSeconds = 500;
-const int nGramIndex = 2;
 const int maxLine = 1;
 const int maxFollowCount = 500;
 // Do not change
+const int nGramIndex = 2;
 const int plusOne = 1;
 const int minusOne = -1;
 const int tenCount = 10;
